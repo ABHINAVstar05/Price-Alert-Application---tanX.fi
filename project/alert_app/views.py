9e08caa99bb1f832e24680773bf0d97138520f6c
Abhinav <kumar.abhinav3125@gmail.com>
2024-02-03 17:27:12 +0530

    Implemented Binance's WebSocket connection to get real-time price updates of BTC

--- project/alert_app/views.py
+++ project/alert_app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import websocket, json
 # Create your views here.
+
+def prices() :
+    socket = f'wss://stream.binance.com:9443/ws/btcusdt@kline_1s'
+
+    def on_message(ws, message):
+        data = json.loads(message)
+        if 'k' in data:
+            data = data['k']
+            if 'c' in data :
+                print("Current price:", data['c'])
+            else:
+                print('Something unexpected occured.')
+        else:
+            print('Something unexpected occured.')
+
+    ws = websocket.WebSocketApp(socket, on_message = on_message)
+    ws.run_forever()
+
+prices()
